f73091a9a4f6b4411769ebaefec11c6901157bbf
tauqeerali1 <tauqeer.ali.9934@gmail.com>
2026-09-09 01:23:08 +0530

    changed

--- launch-ui/components/sections/shared/hero/default.tsx
+++ launch-ui/components/sections/shared/hero/default.tsx
@@ -42,17 +42,7 @@ export default function Hero({
       className="w-full"
     />
   ),
-  badge = (
-    <Badge variant="outline" className="animate-appear">
-      <span className="text-muted-foreground">
-        Automate Your Business in 30 Days
-      </span>
-      <a href={siteConfig.getStartedUrl} className="flex items-center gap-1">
-        Claim Your Slot
-        <ArrowRightIcon className="size-3" />
-      </a>
-    </Badge>
-  ),
+  
   buttons = [
     {
       href: siteConfig.getStartedUrl,
@@ -76,9 +66,7 @@ export default function Hero({
       )}
     >
       <div className="max-w-container mx-auto flex flex-col gap-12 pt-16 sm:gap-24">
-        <div className="flex flex-col items-center gap-6 text-center sm:gap-12">
-          {badge !== false && badge}
-          
+        <div className="flex flex-col items-center gap-6 text-center sm:gap-12">          
           {/* Modified h1 with FlipWords */}
           <h1 className="animate-appear from-foreground to-foreground dark:to-muted-foreground relative z-10 inline-block bg-linear-to-r bg-clip-text text-4xl leading-tight font-semibold text-balance drop-shadow-2xl sm:text-6xl sm:leading-tight md:text-8xl md:leading-tight">
             <span className="flex flex-wrap justify-center items-baseline gap-x-2">
