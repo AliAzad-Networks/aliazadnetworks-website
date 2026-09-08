db0ce646346a5f488b1b85f826f58794bf65b69b
tauqeerali1 <tauqeer.ali.9934@gmail.com>
2026-09-08 22:42:23 +0530

    changed

--- launch-ui/components/sections/shared/hero/default.tsx
+++ launch-ui/components/sections/shared/hero/default.tsx
@@ -44,10 +44,10 @@ export default function Hero({
   ),
   badge = (
     <Badge variant="outline" className="animate-appear">
-      <span className="text-muted-foreground">
+      <span className="text-muted-foreground text-xs sm:text-sm">
         Get your business automated in 30 days
       </span>
-      <a href={siteConfig.getStartedUrl} className="flex items-center gap-1">
+      <a href={siteConfig.getStartedUrl} className="flex items-center gap-1 text-xs sm:text-sm">
         Claim Your Slot
         <ArrowRightIcon className="size-3" />
       </a>
