a65c9b74bfe48e291eda2e0a2e26ad6db7171acc
tauqeerali1 <tauqeer.ali.9934@gmail.com>
2026-09-08 17:57:24 +0530

    changed

--- launch-ui/components/sections/shared/hero/default.tsx
+++ launch-ui/components/sections/shared/hero/default.tsx
@@ -68,10 +68,7 @@ export default function Hero({
   ],
   className,
 }: HeroProps) {
-  // 👇 Define the words that will flip
-  const flipWords = ["Automate", "Innovate", "Scale"];
-
-  return (
+   return (
     <Section
       className={cn(
         "overflow-hidden pb-0 sm:pb-0 md:pb-0",
@@ -85,9 +82,7 @@ export default function Hero({
           {/* Modified h1 with FlipWords */}
           <h1 className="animate-appear from-foreground to-foreground dark:to-muted-foreground relative z-10 inline-block bg-linear-to-r bg-clip-text text-4xl leading-tight font-semibold text-balance drop-shadow-2xl sm:text-6xl sm:leading-tight md:text-8xl md:leading-tight">
             <span className="flex flex-wrap justify-center items-baseline gap-x-2">
-              <span>We Can Help You Reimagine Your Business Through A <span className="inline-block">
-                <FlipWords words={flipWords} duration={1500} className="inline" />
-              </span></span>
+              <span>We Can Help You Reimagine Your Business Through A Innovate</span>
               
             </span>
           </h1>
